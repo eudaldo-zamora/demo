d1d16b470496332d5879c50b263806b245bac0e4
Josselyn <josselynstefania1994@gmail.com>
2022-04-09 15:33:48 -0500

    Email añadido - Josselyn Pita

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,12 +1,18 @@
 package com.example.demo.entity;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import lombok.Data;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity(name = "paske")
 public class User {
 
@@ -20,4 +26,19 @@ public class User {
     @Column(name = "clave")
     private String password;
 
+    @Column(name = "correo")
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
